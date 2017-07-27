c309ea7bf6c72d89c08b38e834332f6ad5295d3f
Michael Madison <cadetstar@hotmail.com>
2017-07-26 21:26:45 -0500

    Fixed aliasing of active admin

--- lib/arctic_admin/active_admin/views/pages/bases.rb
+++ lib/arctic_admin/active_admin/views/pages/bases.rb
@@ -2,9 +2,9 @@ module ActiveAdmin
   module Views
     module Pages
       class Base < Arbre::HTML::Document
-        alias_method :_build_active_admin_head, :build_active_admin_head
+        alias build_active_admin_head_orig build_active_admin_head
         def build_active_admin_head
-          _build_active_admin_head
+          build_active_admin_head_orig
 
           within @head do
             meta name: 'viewport', content: 'width=device-width, initial-scale=1'
